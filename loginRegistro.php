4232c356b806db60f96a4c425bf7d1ac3473a7ad
Mikel <mikelayala1234@gmail.com>
2022-10-11 10:06:25 +0200

    header & footer testing

--- loginRegistro.php
+++ loginRegistro.php
@@ -1,10 +1,23 @@
 <html>
     <head>
-
+        
     </head>
 
     <body>
         <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    include_once("iniciarbd.php");
+    function login(){
+    $user = ["usuario"];
+    $pass = ["pasahitza"];
+    $consultaLogin = $miPDO->prepare("SELECT id_usuario FROM usuarios WHERE   ");
+    $consultaLogin -> execute();
+    }
+
+    function register(){
+
+    }
+}
         
         ?>
         <form action="loginRegistro.php" method="post">
@@ -12,7 +25,8 @@
             <br>
             <p>Pasahitza: <input type="text" name="pasahitza"></p>
             <br>
-            <input type="submit" name="login" value="Saioa Hasi" >
+            <input type="submit" name="login" value="Saioa Hasi" onsubmit="login()">
+            <input type="submit" name="registro" value="Erregistratu" onsubmit="registro()">
         </form>
     </body>
 </html>
