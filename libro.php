f72685f2410d7021691c942719b8c0e348647eb8
Mikel <mikelayala1234@gmail.com>
2022-10-11 11:37:41 +0200

    fixes

---
+++ libro.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/generalStyles.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/libro.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <script src="https://kit.fontawesome.com/9d49876e0a.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <?php include('html/header.html');?>
+</body>
+</html>
